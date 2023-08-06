990f698a2591a32e959d217cd761f9e7ec6d6e7b
M.Wais Malikzada <waismalikzada99@gmail.com>
2023-08-06 08:22:23 +0430

    add login component

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage(){
+    return (
+        <div className="flex justify-center items-center">
+            <h1 className="font-extrabold text-3xl mt-14 text-red-800">Login page</h1>
+        </div>
+    )
+}
