7be1580a510f52d62cf7c5fbdbd729835aedb03d
M.Wais Malikzada <waismalikzada99@gmail.com>
2023-08-17 16:03:19 +0430

    add login and profile front end functionalities

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,47 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { toast } from "react-hot-toast";
 
 export default function LoginPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
   const onLogin = async () => {
-    console.log("Login");
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+      toast.success("Login success");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("SignUp failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="test-center text-white text-2xl">Login Page</h1>
+      <h1 className="test-center text-white text-2xl"> {loading ? "Processing" : "Login Page"}</h1>
       <hr />
       <label htmlFor="email">email</label>
       <input
@@ -41,7 +67,7 @@ export default function LoginPage() {
         onClick={onLogin}
         className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-300"
       >
-        Login
+        {buttonDisabled ? "No Login" : "Login"}
       </button>
       <Link href="/signup">Visit to sign up page</Link>
     </div>
